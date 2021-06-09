bf4f2a63f44bb08fed86ca8068b27660bc350a6d
c-Ruud <caroline.ruud@outlook.com>
2021-06-09 15:45:32 +0200

    fixed some styling  - By Agnes & Caroline

--- components/MainButton.js
+++ components/MainButton.js
@@ -4,24 +4,18 @@ import {
   Text,
   StyleSheet,
   TouchableOpacity,
-  TouchableNativeFeedback,
-  Platform,
 } from 'react-native';
 import Colors from '../constants/colors';
 
 const MainButton = props => {
-  let ButtonComponent = TouchableOpacity;
 
-  if (Platform.OS === 'android' && Platform.Version >= 21) {
-    ButtonComponent = TouchableNativeFeedback;
-  }
   return (
     <View style={styles.buttonContainer}>
-      <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
+      <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
         <View style={styles.button}>
           <Text style={styles.buttonText}>{props.children}</Text>
         </View>
-      </ButtonComponent>
+      </TouchableOpacity>
     </View>
   );
 };
